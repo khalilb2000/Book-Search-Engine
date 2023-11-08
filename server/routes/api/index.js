ba2ce0c543f9cfe877592448363ac0ee9f2d2ef9
khalilb2000 <khalilb2000@gmail.com>
2023-11-08 08:30:19 -0600

    fixed api routes and file placement

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,6 +1,12 @@
 const router = require('express').Router();
-const userRoutes = require('./user-routes');
+const path = require('path');
+const apiRoutes = require('.');
 
-router.use('/users', userRoutes);
+router.use('/api', apiRoutes);
+
+// serve up react front-end in production
+router.use((req, res) => {
+  res.sendFile(path.join(__dirname, '../../client/build/index.html'));
+});
 
 module.exports = router;
